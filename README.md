720bcc5ab80c516a9518bb647eee5d2b096329cd
Marie Stella <addom@moravian.edu>
2024-04-16 18:19:55 -0400

    added resources and explanations to the skelton code in the files

--- README.md
+++ README.md
@@ -7,5 +7,30 @@ cs392 Project Final
 
 
 # Resources
+https://github.com/jimouris/parallel-convolution
+https://github.com/topics/parallel-processing?l=c
+https://github.com/topics/image-convolution
+https://stackoverflow.com/questions/58926559/blurring-an-image-with-c
+https://learnopencv.com/image-filtering-using-convolution-in-opencv/
+https://stackoverflow.com/questions/58252334/convolution-for-image-filtering
+https://medium.com/@henriquevedoveli/image-filtering-techniques-in-image-processing-part-1-d03362fc73b7
+https://ulhpc-tutorials.readthedocs.io/en/latest/cuda/exercises/convolution/
 
 # Will add Project pdf to the project 
+
+Serial with CPU: assumes that you have a function to read the image 
+into a 2D array and a function to write the output image from a 2D array. The 
+convolution operation is performed by a function convolve_serial which 
+takes the 
+image, kernel, and output buffers as arguments along with their 
+dimensions.
+
+Parallel with CPU using OpenMP: uses OpenMP and assumes that you have a 
+function to read the image into a 2D array and a function to write the output image 
+from a 2D array. The convolution operation is performed by a function 
+convolve_parallel which takes the image, kernel, and output buffers as 
+arguments along with their dimensions.
+
+Parallel GPU with CUDA: uses CUDA the convolve_gpu function sets up the 
+memory and calls the kernel. The main function demonstrates how to 
+allocate memory, initialize data, and call the GPU convolution function.
---
+++ convolve_parallel_cpu.c
@@ -0,0 +1,64 @@
+/* This file will run with the CPU in parallel*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+// Function to perform convolution on a single pixel
+float apply_kernel(float **image, float **kernel, int x, int y, int kernel_size) {
+    float sum = 0.0;
+    int offset = kernel_size / 2;
+
+    for (int i = -offset; i <= offset; i++) {
+        for (int j = -offset; j <= offset; j++) {
+            sum += image[x + i][y + j] * kernel[offset + i][offset + j];
+        }
+    }
+
+    return sum;
+}
+
+// Parallel convolution function
+void convolve_parallel(float **image, float **output, float **kernel, int width, int height, int kernel_size) {
+    int offset = kernel_size / 2;
+
+    // Apply kernel to each pixel in parallel
+    #pragma omp parallel for collapse(2)
+    for (int i = offset; i < height - offset; i++) {
+        for (int j = offset; j < width - offset; j++) {
+            output[i][j] = apply_kernel(image, kernel, i, j, kernel_size);
+        }
+    }
+}
+
+int main() {
+    int width, height, kernel_size;
+    float **image, **output, **kernel;
+
+    // Assume functions to read the image into 'image' buffer and kernel into 'kernel' buffer
+    // read_image("path_to_image", &image, &width, &height);
+    // read_kernel("path_to_kernel", &kernel, &kernel_size);
+
+    // Allocate memory for the output buffer
+    output = (float **)malloc(height * sizeof(float *));
+    for (int i = 0; i < height; i++) {
+        output[i] = (float *)malloc(width * sizeof(float));
+    }
+
+    // Perform parallel convolution
+    convolve(image, output, kernel, width, height, kernel_size);
+
+    // Assume function to write the output buffer to an image file
+    // write_image("path_to_output_image", output, width, height);
+
+    // Free allocated memory
+    for (int i = 0; i < height; i++) {
+        free(image[i]);
+        free(output[i]);
+        free(kernel[i]);
+    }
+    free(image);
+    free(output);
+    free(kernel);
+
+    return 0;
+}
